540c459efb685555983048bdf904c9c9c82b0042
Vinicius Robledo <vinicius.robledo@fatec.sp.gov.br>
2020-12-10 23:15:59 -0200

    inclusao ng2-adsense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AdsenseModule } from 'ng2-adsense';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdsenseModule.forRoot({
+      adClient: 'ca-pub-7640562161899788',
+      adSlot: 7259870550})
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
